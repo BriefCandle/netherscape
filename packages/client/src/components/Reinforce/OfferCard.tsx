67d6ee723081369177a4d6572a817bccdf3d06eb
sicalpath <manager@tju.edu.cn>
2023-05-21 17:17:33 +0800

    fix: reinforce list attrs color

--- packages/client/src/components/Reinforce/OfferCard.tsx
+++ packages/client/src/components/Reinforce/OfferCard.tsx
@@ -41,7 +41,7 @@ export const OfferCard = (props:{pcInstance : any}) => {
 
   
   return (
-  <div className={`flex flex-row mt-2 px-2 pt-2 pb-3 bg-white border-2 ${pcInstance.pcClassID == "0x1234" ? "border-green-500" : "border-rose-400"} rounded-lg`}>
+  <div className={`flex flex-row mt-2 pl-2 pr-3 pt-1 pb-3 bg-white border-2 ${pcInstance.pcClassID == "0x1234" ? "border-green-500" : "border-rose-400"} rounded-lg`}>
     <div className="flex flex-col justify-center items-center">
       <div className="text-lg font-bold w-28"> 
         <img src={ pcInstance.pcClassID == "0x1234" ? pc1 : pc4 } className="w-28" />
@@ -49,8 +49,8 @@ export const OfferCard = (props:{pcInstance : any}) => {
       <div className="w-full"> <HPBar hp={pcInstance.hp} maxHP={pcInstance.maxHP}/> </div>
     </div>
     <div className="flex flex-col items-center">
-      <div className="flex flex-col h-28  my-auto justify-center"> 
-        <div className="flex flex-row w-52">
+      <div className="flex flex-col h-28  my-auto justify-center "> 
+        <div className="flex flex-row w-52  mt-3">
           <div className="font-bold">
               Name:
           </div>
@@ -66,20 +66,20 @@ export const OfferCard = (props:{pcInstance : any}) => {
             {pcInstance.owner.substr(0,6)}...{pcInstance.owner.substr(38,42)}
           </div>
         </div>
-        <div className="flex flex-row">
-          <div className="flex">
+        <div className="flex flex-row mt-1">
+          <div className="flex text-red-600">
             <div className="font-bold">
                 ATK:
             </div>
-            <div className="ml-2 font-bold text-gray-500 text-right cursor-pointer ">
+            <div className="ml-2 font-bold text-right cursor-pointer ">
               {pcInstance.atk}
             </div>
           </div>
-          <div className="flex mx-2">
+          <div className="flex mx-2 text-cyan-600">
             <div className="font-bold">
                 SPD:
             </div>
-            <div className="ml-2 font-bold text-gray-500 text-right cursor-pointer ">
+            <div className="ml-2 font-bold text-right cursor-pointer ">
               {pcInstance.atk}
             </div>
           </div>
@@ -87,7 +87,7 @@ export const OfferCard = (props:{pcInstance : any}) => {
             <div className="font-bold">
                 PP:
             </div>
-            <div className="ml-2 font-bold text-gray-500 text-right cursor-pointer ">
+            <div className="ml-2 font-bold text-right cursor-pointer ">
               {pcInstance.atk}
             </div>
           </div>
@@ -95,8 +95,8 @@ export const OfferCard = (props:{pcInstance : any}) => {
         </div>
       </div>
       <div className="flex mt-3 ml-auto "> 
-        <button className="px-2 pb-1 my-auto bg-green-500 hover:bg-green-700 transition ease-in-out delay-75 text-white rounded text-lg font-semibold"> apply </button> 
-        {pcInstance.debtorID == playerEntity && (<button className="ml-2 px-2 pb-1 my-auto bg-orange-500 hover:bg-orange-700 transition ease-in-out delay-75 text-white rounded text-lg font-semibold"> finish </button> )}
+        <button className="px-2 pb-1 my-auto bg-green-500 hover:bg-green-700 transition ease-in-out delay-75 text-white rounded text-lg font-semibold"> Apply </button> 
+        {pcInstance.debtorID == playerEntity && (<button className="ml-2 px-2 pb-1 my-auto bg-orange-500 hover:bg-orange-700 transition ease-in-out delay-75 text-white rounded text-lg font-semibold"> Finish </button> )}
       </div>
     </div>
   </div>
